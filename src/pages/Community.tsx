6491a9acbf13755c6b79ec9b55a1acc458083d39
arowojolu17-spec <arowojolu17@gmail.com>
2026-04-20 21:35:45 +0100

    Update Community.tsx

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -7,7 +7,7 @@ import { MessageCircle, Send } from "lucide-react";
 
 const Community = () => {
   const handleJoinWhatsApp = () => {
-    window.open("https://chat.whatsapp.com/EE0IPvPLr28JqRaHFiNbtM?mode=gi_t", "_blank");
+    window.open("https://chat.whatsapp.com/FRwxPfFKriz0Fo8R6EB7Mr?mode=gi_t", "_blank");
   };
 
   const handleJoinTelegram = () => {
