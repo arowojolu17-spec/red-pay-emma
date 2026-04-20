07a51b91f85e6cfaa0663ad6209b0bc3da13d2f2
arowojolu17-spec <arowojolu17@gmail.com>
2026-04-20 21:34:15 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -10,15 +10,15 @@ import { MessageCircle, Send, Mail, MessagesSquare } from "lucide-react";
 const Support = () => {
   const [liveChatOpen, setLiveChatOpen] = useState(false);
   const handleTelegramSupport = () => {
-    window.open("https://t.me/Redpayagent1", "_blank");
+    window.open("t.me/Redpayltd89", "_blank");
   };
 
   const handleWhatsAppSupport = () => {
-    window.open("https://wa.me/234+2349070973852", "_blank");
+    window.open("https://wa.me/2348152859535", "_blank");
   };
 
   const handleEmailSupport = () => {
-    window.location.href = "mailto:donbo7794@gmail.com";
+    window.location.href = "mailto:redpaylimited.98@gmail.com";
   };
 
   const handleLiveChat = () => {
